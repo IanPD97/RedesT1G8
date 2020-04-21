8ed925b3f901ec37792769c2d7a737d5dd9ecd9d
Sebastian <sebastian.perezb@utem.cl>
2020-04-20 20:05:07 -0400

    creacion servidor SOAP

---
+++ ServidorSOAP/app.js
@@ -0,0 +1,43 @@
+const soap = require('soap');
+const xml = require('fs').readFileSync('esquemaServicio.wsdl', 'utf8');
+const express = require('express');
+
+const { verificarDigito, saludar } = require('./modules/servicios');
+
+const error = (nombreFuncion, subCodigo, texto) => {
+  throw {
+    Fault: {
+      Code: {
+        Value: nombreFuncion,
+        Subcode: { value: subCodigo }
+      },
+      Reason: { Text: texto }
+    }
+  };
+}
+
+let servicio = {
+  servicio: {
+    puerto: {
+      digitoVerificador: ({ rut }) => {
+        if (rut === null) error('digitoVerificador', 'rpc:BadArguments', 'Rut invalido');
+        return verificarDigito({ rut });
+      },
+      saludo: ({ sexo, nombre, apellidoPaterno, apellidoMaterno }) => {
+        if (nombre === null
+          || apellidoPaterno === null
+          || apellidoMaterno === null
+          || sexo === null) error('saludo', 'rpc:BadArguments', 'Al menos un parametro es invalido');
+        return saludar({ sexo, nombre, apellidoPaterno, apellidoMaterno });
+      }
+    }
+  }
+};
+
+
+var app = express();
+app.listen(8001, function () {
+  soap.listen(app, '/esquemaServicio', servicio, xml, function () {
+    console.log('server initialized');
+  });
+});
